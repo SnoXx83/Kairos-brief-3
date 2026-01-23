5f67621dad243826534f4ddd7e9cfea547e0e73d
maxime <maximelarquetoux@gmail.com>
2026-01-23 11:28:58 +0100

    Navbar added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import logo from "../assets/Kairos.png" 
 
 export default function Nav() {
   return (
-    <div>
-        navbar
+    <div className="navbar py- px-10 flex justify-around items-center  bg-base-100 shadow-sm  max-sm:flex-col">
+      <div className="flex items-center py-4">
+        <a className="btn h-20  btn-ghost text-xl">
+          <img className="h-20 w-auto object-contain" src={logo} alt="logo de Kairos" />
+        </a>
+        
+      </div>
+      <div className="flex gap-10 items-center">
+        <a className="hover:text-primary cursor-pointer">Accueil</a>
+        <input type="text" placeholder="Search" className="input input-bordered w-full max-w-xs"/>
+      </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,7 @@ import Nav from '../components/Nav'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl font-extrabold tracking-tight text-slate-900"> Kairos </h1>
       <Nav />
-
     </div>
   )
 }
